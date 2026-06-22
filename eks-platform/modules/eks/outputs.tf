93ee3ffea7c7803d4a1361ed4ca3f6227dcce6ca
Schlenkian05 <carthickpalraj@gmail.com>
2026-06-23 03:47:06 +0530

    Update outputs.tf

--- eks-platform/modules/eks/outputs.tf
+++ eks-platform/modules/eks/outputs.tf
@@ -9,3 +9,11 @@ output "cluster_endpoint" {
 output "cluster_arn" {
   value = module.eks.cluster_arn
 }
+
+output "oidc_provider_arn" {
+  value = module.eks.oidc_provider_arn
+}
+
+output "cluster_oidc_issuer_url" {
+  value = module.eks.cluster_oidc_issuer_url
+}
