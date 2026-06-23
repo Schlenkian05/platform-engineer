2e6f996f58f71c0f70ea6ffa50b72a7d8d270ee4
Schlenkian05 <carthickpalraj@gmail.com>
2026-06-23 06:13:06 +0530

    Update outputs.tf

--- eks-platform/modules/nat-instance/outputs.tf
+++ eks-platform/modules/nat-instance/outputs.tf
@@ -1,15 +1,7 @@
-output "nat_instance_id" {
-  value = aws_instance.nat.id
+output "nat_gateway_id" {
+  value = aws_nat_gateway.main.id
 }
 
-output "nat_private_ip" {
-  value = aws_instance.nat.private_ip
-}
-
-output "nat_public_ip" {
-  value = aws_instance.nat.public_ip
-}
-
-output "nat_security_group_id" {
-  value = aws_security_group.nat_sg.id
+output "nat_gateway_eip" {
+  value = aws_eip.nat.public_ip
 }
